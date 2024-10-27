8b8ceb7faaca5f1cb737d7c00b106078ecee8d0f
fs.anvr <fs.anvr@gmail.com>
2024-10-27 18:58:41 +1000

    Car and Comment models

--- carsite/showcase/models.py
+++ carsite/showcase/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=64)
+    model = models.CharField(max_length=64)
+    year = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.make} {self.model} {self.year}"
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    car = models.ForeignKey(Car, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.car}"
+
